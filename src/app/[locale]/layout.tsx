21dae81105022edb9e23cb8f99f36d27938bc3f8
oceane.pezennec.henry <oceane.pezennec.henry@gmail.com>
2025-06-02 21:59:02 +0200

    Fix layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
